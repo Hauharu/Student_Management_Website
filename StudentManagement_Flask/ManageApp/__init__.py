8361d927f84f5ecd4435415ba29393c6ec770ff7
Nguyễn Trung Hậu <138142163+Hauharu@users.noreply.github.com>
2024-12-03 00:01:52 +0700

    Update __init__.py

--- StudentManagement_Flask/ManageApp/__init__.py
+++ StudentManagement_Flask/ManageApp/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 
-app.config["SQLALCHEMY_DATABASE_URI"] ="mysql+pymysql://root:240204@localhost/table_test?charset=utf8mb4"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:%s@localhost/table_test?charset=utf8mb4" % quote('12345678@')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
 db = SQLAlchemy(app)
