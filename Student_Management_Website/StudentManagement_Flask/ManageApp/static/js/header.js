d1864202c80c4c55a0849090193c71319ec9278f
Nguyễn Trung Hậu <138142163+Hauharu@users.noreply.github.com>
2024-12-10 11:15:47 +0700

    Delete Student_Management_Website directory

--- Student_Management_Website/StudentManagement_Flask/ManageApp/static/js/header.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    var main_route = (window.location.pathname.split("/")[1]);
-    $('.main').removeClass('active');
-    $('#nav_' + main_route).addClass('active');
-    $(document).on('click', '.main', function (e) {
-        $('.main').removeClass('active');
-        $('#nav_' + main_route).addClass('active');
-    })
-})
