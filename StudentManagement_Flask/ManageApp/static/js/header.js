d8903d0c9fba8cbc7cbfd32ec46643f3241bf3e1
Nguyễn Trung Hậu <138142163+Hauharu@users.noreply.github.com>
2024-12-21 01:19:34 +0700

    Update_lan2

--- StudentManagement_Flask/ManageApp/static/js/header.js
+++ StudentManagement_Flask/ManageApp/static/js/header.js
@@ -1,9 +1,11 @@
+/*Làm nổi bật mục của menu tương ứng với url hiện tại và cập nhật trạng thái của mục khi người dùng nhấn vào mục khác của menu */
+
 $(document).ready(function() {
     var main_route = (window.location.pathname.split("/")[1]);
     $('.main').removeClass('active');
     $('#nav_' + main_route).addClass('active');
     $(document).on('click', '.main', function (e) {
-        $('.main').removeClass('active');
-        $('#nav_' + main_route).addClass('active');
+    $('.main').removeClass('active');
+    $('#nav_' + main_route).addClass('active');
     })
 })
