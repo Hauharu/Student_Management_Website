40a6bcd7f21da530270ac68d4ab29016a3c2d6c5
Nguyễn Trung Hậu <138142163+Hauharu@users.noreply.github.com>
2024-12-21 13:29:50 +0700

    Add files via upload

--- StudentManagement_Flask/ManageApp/models.py
+++ StudentManagement_Flask/ManageApp/models.py
@@ -40,7 +40,7 @@ class UserInformation(db.Model):
     gender = Column(Enum(UserGender))
     dateOfBirth = Column(Date, nullable=False)
     address = Column(String(255), nullable=False)
-    phoneNumber = Column(String(15), nullable=False, unique=True)
+    phone_Number = Column(String(15), nullable=False, unique=True)
     email = Column(String(255), nullable=False, unique=True)
     joined_date = Column(DateTime, default=datetime.now())
     is_active = Column(Boolean, default=True)
@@ -74,12 +74,23 @@ class UserType(db.Model):
     student = Column(Boolean, nullable=False)
 
 
-# Bảng Age
-class Age(db.Model):
-    __tablename__ = 'Age'
-    ageID = Column(Integer, primary_key=True, autoincrement=True)
-    minAge = Column(Integer, nullable=False)
-    maxAge = Column(Integer, nullable=False)
+# Bảng Teacher
+class Teacher(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    qualification = Column(String(20))
+
+    user_id = Column(Integer, ForeignKey(User.id), primary_key=True)
+
+
+# teach = relationship('Teach', backref='teacher', lazy=True)
+
+
+# form_teacher = relationship('FormTeacher', backref='teacher', uselist=False, lazy=True)
+
+
+# Bảng Staff
+class Staff(User):
+    user_id = Column(Integer, ForeignKey(User.id), primary_key=True)
 
 
 # Bảng Student
@@ -87,37 +98,6 @@ class Student(UserInformation):
     # admission_date = Column(DateTime, default=datetime.now())
     id = Column(Integer, primary_key=True, autoincrement=True)
     student_class = relationship('StudentClass', backref='student', lazy=True)
-
-
-# Bảng Admin
-class Admin(db.Model):
-    __tablename__ = 'Admin'
-    adminID = Column(Integer, ForeignKey(User.id), primary_key=True)
-
-
-# Bảng Teacher
-class Teacher(db.Model):
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    qualification = Column(String(20))
-    user_id = Column(Integer, ForeignKey(User.id), primary_key=True)
-
-
-# teach = relationship('Teach', backref='teacher', lazy=True)
-# form_teacher = relationship('FormTeacher', backref='teacher', uselist=False, lazy=True)
-
-# Bảng Staff
-class Staff(User):
-    user_id = Column(Integer, ForeignKey(User.id), primary_key=True)
-
-
-# Bảng PhoneNumber
-class PhoneNumber(db.Model):
-    __tablename__ = 'PhoneNumber'
-    phoneNumberID = Column(Integer, primary_key=True, autoincrement=True)
-    value = Column(String(15), nullable=False)
-    studentID = Column(Integer, ForeignKey('Student.studentID'), nullable=True)
-    teacherID = Column(Integer, ForeignKey('Teacher.teacherID'), nullable=True)
-    schoolStaffID = Column(Integer, ForeignKey('SchoolStaff.staffID'), nullable=True)
 
 
 # Bảng Class
@@ -133,34 +113,6 @@ class Class(db.Model):
         return self.name
 
 
-# Bảng StudentGrade
-class StudentGrade(db.Model):
-    __tablename__ = 'StudentGrade'
-    gradeID = Column(Integer, primary_key=True, autoincrement=True)
-    grade_10 = Column(Boolean, nullable=False)
-    grade_11 = Column(Boolean, nullable=False)
-    grade_12 = Column(Boolean, nullable=False)
-
-
-# Bảng Subject
-class Subject(db.Model):
-    __tablename__ = 'Subject'
-    subjectID = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(255), nullable=False)
-    exam_15mins = Column(Float, nullable=False)
-    exam_45mins = Column(Float, nullable=False)
-    finalExam = Column(Float, nullable=False)
-
-
-# Bảng ScoreType
-class ScoreType(db.Model):
-    __tablename__ = 'ScoreType'
-    scoreTypeID = Column(Integer, primary_key=True, autoincrement=True)
-    exam_15mins = Column(Boolean, nullable=False)
-    exam_45mins = Column(Boolean, nullable=False)
-    finalExam = Column(Boolean, nullable=False)
-
-
 # Bảng Period
 class Period(db.Model):
     id = Column(Integer, primary_key=True, autoincrement=True)
@@ -171,62 +123,19 @@ class Period(db.Model):
     #  form_teacher = relationship('FormTeacher', backref='period', uselist=False, lazy=True)
     student_class = relationship('StudentClass', backref='period', lazy=True)
 
-    __table_args__ = (UniqueConstraint('semester', 'year', name='unique_semester_year'),)
+    __table_args__ = (
+        UniqueConstraint('semester', 'year', name='unique_semester_year'),
+    )
 
     def __str__(self):
         return f'{"Học kì 1" if self.semester == Semester.SEMESTER_1 else "Học kì 2"} {self.year}'
+
 
 # Bảng StudentClass
 class StudentClass(db.Model):
     student_id = Column(Integer, ForeignKey(Student.id), primary_key=True)
     class_id = Column(Integer, ForeignKey(Class.id), primary_key=True)
     period_id = Column(Integer, ForeignKey(Period.id), primary_key=True)
-
-# Bảng Semester
-class Semester(db.Model):
-    __tablename__ = 'Semester'
-    semesterID = Column(Integer, primary_key=True, autoincrement=True)
-    semester_1 = Column(Boolean, nullable=False)
-    semester_2 = Column(Boolean, nullable=False)
-
-
-# Bảng Score
-class Score(db.Model):
-    __tablename__ = 'Score'
-    scoreID = Column(Integer, primary_key=True, autoincrement=True)
-    studentID = Column(Integer, ForeignKey('Student.studentID'), nullable=False)
-    subjectID = Column(Integer, ForeignKey('Subject.subjectID'), nullable=False)
-    periodID = Column(Integer, ForeignKey('Period.periodID'), nullable=False)
-
-    student = relationship('Student', backref='scores', lazy=True)
-    subject = relationship('Subject', backref='scores', lazy=True)
-    period = relationship('Period', backref='scores', lazy=True)
-
-
-# Bảng ScoreDetail
-class ScoreDetail(db.Model):
-    __tablename__ = 'ScoreDetail'
-    scoreDetailID = Column(Integer, primary_key=True, autoincrement=True)
-    score = Column(Float, nullable=False)
-    scoreTypeID = Column(Integer, ForeignKey('ScoreType.scoreTypeID'), nullable=False)
-    scoreID = Column(Integer, ForeignKey('Score.scoreID'), nullable=False)
-
-    score_type = db.relationship('ScoreType', backref='score_details', lazy=True)
-    scores = relationship('Score', backref='score_details', lazy=True)
-
-
-# Bảng Class_Teacher
-class ClassTeacher(db.Model):
-    __tablename__ = 'Class_Teacher'
-    classID = Column(Integer, ForeignKey('Class.classID'), primary_key=True)
-    teacherID = Column(Integer, ForeignKey('Teacher.teacherID'), primary_key=True)
-
-
-# Bảng Teacher_Subject
-class TeacherSubject(db.Model):
-    __tablename__ = 'Teacher_Subject'
-    teacherID = Column(Integer, ForeignKey('Teacher.teacherID'), primary_key=True)
-    subjectID = Column(Integer, ForeignKey('Subject.subjectID'), primary_key=True)
 
 
 if __name__ == "__main__":
@@ -241,15 +150,15 @@ if __name__ == "__main__":
         # c7 = Class(name='12A2', grade=Grade.K12)
         # c8 = Class(name='12B3', grade=Grade.K12)
         # c9 = Class(name='12C2', grade=Grade.K12)
-
+        #
         # db.session.add_all([c1, c2, c3, c4, c5, c6, c7, c8, c9])
         # db.session.commit()
-
+        #
         # p1 = Period(semester=Semester.S1, year='2024')
         # p2 = Period(semester=Semester.S2, year='2024')
         # db.session.add_all([p1, p2])
         # db.session.commit()
-
+        #
         # # thêm học sinh vào lớp
         # sc = StudentClass(student_id=3, class_id=1)
         # sc1 = StudentClass(student_id=4, class_id=1)
@@ -278,7 +187,7 @@ if __name__ == "__main__":
         #
         # db.session.add_all([sc, sc1, sc2, sc3, sc4, sc5, sc6, sc7, sc8, sc9])
         # db.session.commit()
-
+        #
         # import json
         #
         # with open('data/students.json', encoding="utf-8") as f:
@@ -289,7 +198,7 @@ if __name__ == "__main__":
         #                        phoneNumber=s['phone_number'], email=s['email'], avatar=s['avatar'])
         #         db.session.add(stud)
         #     db.session.commit()
-
+        #
         # import hashlib
         #
         # u1 = User(
@@ -299,7 +208,7 @@ if __name__ == "__main__":
         #     dateOfBirth=datetime.strptime('2004-01-01', '%Y-%m-%d').date(),
         #     password=str(hashlib.md5('123'.encode('utf8')).hexdigest()),
         #     user_role=UserRole.ADMIN,
-        #     phoneNumber='12345678910',
+        #     phone_Number='12345678910',
         #     address='quan 1',
         #     gender=UserGender.MALE
         # )
@@ -311,7 +220,7 @@ if __name__ == "__main__":
         #     dateOfBirth=datetime.strptime('2004-01-01', '%Y-%m-%d').date(),
         #     password=str(hashlib.md5('123'.encode('utf8')).hexdigest()),
         #     user_role=UserRole.TEACHER,
-        #     phoneNumber='12345678321',
+        #     phone_Number='12345678321',
         #     address='quan 2',
         #     gender=UserGender.MALE
         # )
@@ -323,7 +232,7 @@ if __name__ == "__main__":
         #     dateOfBirth=datetime.strptime('2004-01-01', '%Y-%m-%d').date(),
         #     password=str(hashlib.md5('123'.encode('utf8')).hexdigest()),
         #     user_role=UserRole.STAFF,
-        #     phoneNumber='12345678123',
+        #     phone_Number='12345678123',
         #     address='quan 1',
         #     gender=UserGender.MALE
         # )
