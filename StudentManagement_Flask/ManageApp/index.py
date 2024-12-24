0fea549b7809eb9a92fc0afba89c6ff59a01d274
Nguyễn Trung Hậu <138142163+Hauharu@users.noreply.github.com>
2024-12-24 16:20:21 +0700

    Add files via upload
    update_lan7

--- StudentManagement_Flask/ManageApp/index.py
+++ StudentManagement_Flask/ManageApp/index.py
@@ -1,7 +1,12 @@
+import flash
 from ManageApp import app, controller, login, dao
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, jsonify
 from flask_login import login_user,logout_user, login_required  # Hỗ trợ xác thực người dùng
+from wtforms.validators import email
 from ManageApp.models import *
+
+
+app.secret_key="Admin@123"
 
 # Định tuyến cho trang chủ
 app.add_url_rule("/", 'index', controller.index)
@@ -38,6 +43,42 @@ def user_login():
 def user_logout():
     logout_user()
     return  redirect(url_for(user_login))
+
+#Chức năng Staff
+@app.route('/staff')
+@login_required
+def staff():
+    return render_template('staff/staff.html')
+
+@app.route('/tiepnhan', methods=['GET', 'POST'])
+def tiepnhan():
+    err_msg=""
+    if request.method.__eq__('POST'):
+        name=request.form.get("name")
+        gender=request.form.get("gender")
+        dateOfBirth=request.form.get("dateOfBirth")
+        address=request.form.get("address")
+        phoneNumber=request.form.get("phoneNumber")
+        email=request.form.get("email")
+        admission_date=request.form.get("admission_date")
+        class_id=request.form.get("class_id")
+    return render_template('staff/tiepnhan.html')
+
+
+
+@app.route('/dshocsinh')
+@login_required
+def dshocsinh():
+    kw = request.args.get('kw')
+    student = dao.get_student_list()
+    return render_template('staff/dshocsinh.html', student=student)
+
+@app.route('/dslop', methods=['GET', 'POST'])
+def dslophoc():
+    kw = request.args.get('kw')
+    class_list=dao.get_class_list()
+    return render_template('staff/dslophoc.html', class_list=class_list)
+
 
 if __name__ == "__main__":
     from ManageApp.admin import *
