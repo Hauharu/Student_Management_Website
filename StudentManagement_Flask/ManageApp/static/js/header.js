4dc86f7f75755f47a9a86e36b213f08a40c8e3b2
Nguyễn Trung Hậu <138142163+Hauharu@users.noreply.github.com>
2025-01-02 22:21:10 +0700

    Add files via upload

---
+++ StudentManagement_Flask/ManageApp/static/js/header.js
@@ -0,0 +1,12 @@
+/*Làm nổi bật mục của menu tương ứng với url hiện tại và cập nhật trạng thái của mục khi người dùng nhấn vào mục khác của menu */
+
+$(document).ready(function() {
+    var main_route = (window.location.pathname.split("/")[1]);
+    $('.main').removeClass('active');
+    $('#nav_' + main_route).addClass('active');
+    $(document).on('click', '.main', function (e) {
+    $('.main').removeClass('active');
+    $('#nav_' + main_route).addClass('active');
+    })
+})
+
