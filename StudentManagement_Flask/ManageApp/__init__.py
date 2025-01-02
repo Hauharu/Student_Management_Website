f8e57b9912908d320219a66d9bf1c190cb49206b
Nguyễn Trung Hậu <138142163+Hauharu@users.noreply.github.com>
2025-01-02 22:23:23 +0700

    Add files via upload

---
+++ StudentManagement_Flask/ManageApp/__init__.py
@@ -0,0 +1,43 @@
+import cloudinary
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from urllib.parse import quote
+from flask_login import LoginManager
+from flask_babelex import Babel
+from flask_mail import Mail, Message
+
+
+app = Flask(__name__)
+
+
+app.secret_key = '@#$%^&*(#@#$%^&@#$%^&*@#$&%$@##$^!@#$%^&)(*&^%$#@!$%^'
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:%s@localhost/manage_db?charset=utf8mb4" % quote('12345678@')
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+app.config["CN_PAGE_SIZE"] = 6
+
+
+app.config['MAIL_SERVER'] = "smtp.gmail.com"
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USERNAME'] = "@gmail.com"
+app.config['MAIL_PASSWORD'] =os.getenv("yourpassword")
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+
+
+cloudinary.config(
+    cloud_name='dwwfgtxv4',
+    api_key='847843234855491',
+    api_secret='OEbZdz4wwMCsG_CEfXW6ScQFliI'
+)
+
+
+db = SQLAlchemy(app=app)
+login = LoginManager(app=app)
+babel = Babel(app=app)
+mail = Mail(app)
+
+
+@babel.localeselector
+def get_locale():
+    return 'vi'
